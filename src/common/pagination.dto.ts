86c6ac9e098f60df3428b51cbdd6a653efbe53f1
jvnakad <joaonakad@gmail.com>
2026-03-26 16:02:26 -0300

    feat: add swagger docs to pagination query params

--- src/common/pagination.dto.ts
+++ src/common/pagination.dto.ts
@@ -1,13 +1,16 @@
 import { IsOptional, IsInt, Min } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class PaginationDto {
+  @ApiPropertyOptional({ default: 1, description: 'Número da página' })
   @IsOptional()
   @Type(() => Number)
   @IsInt()
   @Min(1)
   page?: number = 1;
 
+  @ApiPropertyOptional({ default: 10, description: 'Itens por página' })
   @IsOptional()
   @Type(() => Number)
   @IsInt()
